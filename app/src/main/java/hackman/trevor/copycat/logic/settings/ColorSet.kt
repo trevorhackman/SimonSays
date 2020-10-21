4da8edab7f44e0367dd24ee73c365f0f5bd7051a
Trevor Hackman <trevorhackman@gmail.com>
2020-10-21 00:05:56 -0400

    50th Commit
    Reordering color sets & new blacked color set

--- app/src/main/java/hackman/trevor/copycat/logic/settings/ColorSet.kt
+++ app/src/main/java/hackman/trevor/copycat/logic/settings/ColorSet.kt
@@ -9,13 +9,14 @@ enum class ColorSet(
     val colors: ColorQuad
 ) : NameId {
     Classic(R.string.settings_color_classic, classicColors),
+    Soft(R.string.settings_color_soft, softColors),
+    Inverted(R.string.settings_color_inverted, invertedColors),
+    Dull(R.string.settings_color_dull, dullColors),
     Warm(R.string.settings_color_warm, warmColors),
     Cool(R.string.settings_color_cool, coolColors),
     Royal(R.string.settings_color_royal, royalColors),
-    Inverted(R.string.settings_color_inverted, invertedColors),
-    Dull(R.string.settings_color_dull, dullColors),
-    Space(R.string.settings_color_soft, softColors),
-    Greyed(R.string.settings_color_greyed, greyedColors);
+    Greyed(R.string.settings_color_greyed, greyedColors),
+    Blacked(R.string.settings_color_blacked, blackedColors);
 
     override val nameId = StringResource(id)
 }
@@ -37,6 +38,15 @@ data class ColorQuad(
 private val classicColors =
     ColorQuad(R.color.classic0, R.color.classic1, R.color.classic2, R.color.classic3)
 
+private val softColors =
+    ColorQuad(R.color.soft0, R.color.soft1, R.color.soft2, R.color.soft3)
+
+private val invertedColors =
+    ColorQuad(R.color.inverted0, R.color.inverted1, R.color.inverted2, R.color.inverted3)
+
+private val dullColors =
+    ColorQuad(R.color.dull0, R.color.dull1, R.color.dull2, R.color.dull3)
+
 private val warmColors =
     ColorQuad(R.color.warm0, R.color.warm1, R.color.warm2, R.color.warm3)
 
@@ -46,14 +56,8 @@ private val coolColors =
 private val royalColors =
     ColorQuad(R.color.royal0, R.color.royal1, R.color.royal2, R.color.royal3)
 
-private val invertedColors =
-    ColorQuad(R.color.inverted0, R.color.inverted1, R.color.inverted2, R.color.inverted3)
-
-private val dullColors =
-    ColorQuad(R.color.dull0, R.color.dull1, R.color.dull2, R.color.dull3)
-
-private val softColors = 
-    ColorQuad(R.color.soft0, R.color.soft1, R.color.soft2, R.color.soft3)
-
 private val greyedColors =
     ColorQuad(R.color.greyed, R.color.greyed, R.color.greyed, R.color.greyed)
+
+private val blackedColors =
+    ColorQuad(R.color.blacked, R.color.blacked, R.color.blacked, R.color.blacked)
