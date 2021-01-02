328ed0de9b512b4c61b7321f9c985098571cd1fb
Trevor <trevorhackman@gmail.com>
2021-01-01 21:15:46 -0500

    58th Commit
    Very rarely, a button press to open a dialog goes off, activity is destroyed in the meantime, and then DialogFactory.kt crashes from not having an activity.
    
    Crash fixed. In such rare cases, dialog just won't open. Shouldn't matter since activity is destroyed.

--- app/src/main/java/hackman/trevor/copycat/ui/DialogFactory.kt
+++ app/src/main/java/hackman/trevor/copycat/ui/DialogFactory.kt
@@ -24,80 +24,74 @@ object DialogFactory: LifecycleObserver {
         activity = null
     }
 
+    // Rarely activity can be destroyed after a button is pressed but before callback, previously crashing here
     @Suppress("DEPRECATION") // THEME_HOLO_DARK is dank
-    private fun buildStyle() = AlertDialog.Builder(activity, THEME_HOLO_DARK)
+    private fun buildStyle() = activity?.let { AlertDialog.Builder(it, THEME_HOLO_DARK) }
 
-    fun leaveCurrentGame(onExit: () -> Unit, onMainMenu: () -> Unit): AlertDialog =
+    fun leaveCurrentGame(onExit: () -> Unit, onMainMenu: () -> Unit): AlertDialog? =
         buildStyle()
-            .setTitle(R.string.dialog_exit_title)
-            .setMessage(R.string.dialog_exit_message)
-            .setPositiveButton(R.string.dialog_exit_button) { _, _ -> onExit() }
-            .setNeutralButton(R.string.dialog_exit_main_menu_button) { _, _ -> onMainMenu() }
-            .setNegativeButton(R.string.dialog_cancel_button, null)
-            .create()
+            ?.setTitle(R.string.dialog_exit_title)
+            ?.setMessage(R.string.dialog_exit_message)
+            ?.setPositiveButton(R.string.dialog_exit_button) { _, _ -> onExit() }
+            ?.setNeutralButton(R.string.dialog_exit_main_menu_button) { _, _ -> onMainMenu() }
+            ?.setNegativeButton(R.string.dialog_cancel_button, null)
+            ?.create()
 
-    fun viewMoreGames(onViewMoreGames: () -> Unit): AlertDialog =
+    fun viewMoreGames(onViewMoreGames: () -> Unit): AlertDialog? =
         buildStyle()
-            .setTitle(R.string.dialog_more_games_title)
-            .setMessage(R.string.dialog_more_games_message)
-            .setPositiveButton(R.string.dialog_more_games_view_button) { _, _ -> onViewMoreGames() }
-            .setNegativeButton(R.string.dialog_cancel_button, null)
-            .create()
+            ?.setTitle(R.string.dialog_more_games_title)
+            ?.setMessage(R.string.dialog_more_games_message)
+            ?.setPositiveButton(R.string.dialog_more_games_view_button) { _, _ -> onViewMoreGames() }
+            ?.setNegativeButton(R.string.dialog_cancel_button, null)
+            ?.create()
 
-    fun rateTheApp(onRate: () -> Unit): AlertDialog =
+    fun rateTheApp(onRate: () -> Unit): AlertDialog? =
         buildStyle()
-            .setTitle(R.string.dialog_rate_app_title)
-            .setMessage(R.string.dialog_rate_app_message)
-            .setPositiveButton(R.string.dialog_rate_app_rate_button) { _, _ -> onRate() }
-            .setNegativeButton(R.string.dialog_cancel_button, null)
-            .create()
+            ?.setTitle(R.string.dialog_rate_app_title)
+            ?.setMessage(R.string.dialog_rate_app_message)
+            ?.setPositiveButton(R.string.dialog_rate_app_rate_button) { _, _ -> onRate() }
+            ?.setNegativeButton(R.string.dialog_cancel_button, null)
+            ?.create()
 
-    fun purchaseMenu(onPurchase: () -> Unit): AlertDialog =
+    fun successfulNoAdsPurchase(): AlertDialog? =
         buildStyle()
-            .setTitle(R.string.dialog_no_ads_title)
-            .setMessage(R.string.dialog_no_ads_message)
-            .setPositiveButton(R.string.dialog_no_ads_button) { _, _ -> onPurchase() }
-            .setNegativeButton(R.string.dialog_cancel_button, null)
-            .create()
+            ?.setTitle(R.string.dialog_purchase_success_title)
+            ?.setMessage(R.string.dialog_purchase_success_message)
+            ?.setNeutralButton(R.string.dialog_ok_button, null)
+            ?.create()
 
-    fun successfulNoAdsPurchase(): AlertDialog =
+    fun noAdsAlreadyPurchased(): AlertDialog? =
         buildStyle()
-            .setTitle(R.string.dialog_purchase_success_title)
-            .setMessage(R.string.dialog_purchase_success_message)
-            .setNeutralButton(R.string.dialog_ok_button, null)
-            .create()
+            ?.setTitle(R.string.dialog_already_purchased_title)
+            ?.setMessage(R.string.dialog_already_purchased_message)
+            ?.setNeutralButton(R.string.dialog_ok_button, null)
+            ?.create()
 
-    fun noAdsAlreadyPurchased(): AlertDialog =
+    fun failedNetwork(): AlertDialog? =
         buildStyle()
-            .setTitle(R.string.dialog_already_purchased_title)
-            .setMessage(R.string.dialog_already_purchased_message)
-            .setNeutralButton(R.string.dialog_ok_button, null)
-            .create()
+            ?.setTitle(R.string.dialog_failed_network_title)
+            ?.setMessage(R.string.dialog_failed_network_message)
+            ?.setNeutralButton(R.string.dialog_ok_button, null)
+            ?.create()
 
-    fun failedNetwork(): AlertDialog =
+    fun billingUnavailable(): AlertDialog? =
         buildStyle()
-            .setTitle(R.string.dialog_failed_network_title)
-            .setMessage(R.string.dialog_failed_network_message)
-            .setNeutralButton(R.string.dialog_ok_button, null)
-            .create()
+            ?.setTitle(R.string.dialog_billing_unavailable_title)
+            ?.setMessage(R.string.dialog_billing_unavailable_message)
+            ?.setNeutralButton(R.string.dialog_ok_button, null)
+            ?.create()
 
-    fun billingUnavailable(): AlertDialog =
+    fun unknownError(errorMessage: String): AlertDialog? =
         buildStyle()
-            .setTitle(R.string.dialog_billing_unavailable_title)
-            .setMessage(R.string.dialog_billing_unavailable_message)
-            .setNeutralButton(R.string.dialog_ok_button, null)
-            .create()
-
-    fun unknownError(errorMessage: String): AlertDialog =
-        buildStyle()
-            .setTitle(R.string.dialog_unknown_error_title)
-            .setMessage(errorMessage)
-            .setNeutralButton(R.string.dialog_ok_button, null)
-            .create()
+            ?.setTitle(R.string.dialog_unknown_error_title)
+            ?.setMessage(errorMessage)
+            ?.setNeutralButton(R.string.dialog_ok_button, null)
+            ?.create()
 }
 
 // Don't show status bar when dialog appears. Dialogs are so overly complicated. Foogle.
-fun AlertDialog.showCorrectly() {
+fun AlertDialog?.showCorrectly() {
+    this ?: return
     window?.setFlags(WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE, WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE)
     show()
 }
